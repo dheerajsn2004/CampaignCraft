c8b48167b6c4346ce31536940502cd145baad6b8
dheerajsn2004 <sndheeraj2004@gmail.com>
2025-03-07 09:49:32 +0530

    final

--- main.py
+++ main.py
@@ -147,4 +147,4 @@ async def get_models():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
